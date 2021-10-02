120370d4ac18b024292fd9bf820d4b37985fa2c8
IDepp3 <alvarez.joaquinez@gmail.com>
2021-10-02 07:56:51 -0300

    Fixeamos action home form <Login>

--- views/home.php
+++ views/home.php
@@ -5,7 +5,7 @@
       <img src="<?php echo IMG_PATH?>utn.png" class="card-img-top" alt="logo utn">
     </div>
     <div class="card-body h-25">
-      <form action="" method="POST" class="d-flex justify-content-center flex-column" autocomplete="off">
+      <form action="<?php FRONT_ROOT ?>student/login" method="POST" class="d-flex justify-content-center flex-column" autocomplete="off">
         <div class="mb-3 w-100 d-flex flex-column justify-content-center">
           <label for="email" class="form-label text-center label__p">Email</label>
           <input type="email" name="email" id="email" class="form-control align-self-center input__p"/>
